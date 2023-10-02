56a4b758142224b909f3ceba8b0ae99f86c5b52d
JonniBig <bigun.i88@gmail.com>
2023-10-02 14:46:50 +0300

    delete_did

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,18 +15,6 @@ export class App extends Component {
     ],
     filter: '',
   };
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts) {
-      this.setState({ contacts: JSON.parse(savedContacts) });
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
 
   handleAddContact = (name, number) => {
     const isNameExists = this.state.contacts.some(
